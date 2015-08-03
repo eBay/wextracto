d4dceecdf9e19cc74e22456248eb4b376744092a
Giles Brown <gsbrown@ebay.com>
2015-08-03 14:21:02 -0700

    Suppress XMLSyntaxError when reading sitemaps

--- wex/sitemaps.py
+++ wex/sitemaps.py
@@ -5,13 +5,16 @@ and `sitemaps <http://www.sitemaps.org/protocol.html>`_.
 
 
 from __future__ import unicode_literals, absolute_import, print_function
+import logging
 import wex.py2compat ; assert wex.py2compat
-from lxml.etree import iterparse
+from lxml.etree import iterparse, XMLSyntaxError
 from codecs import getreader
 from six.moves.urllib_parse import urljoin
 from wex.extractor import chained
 from wex.http import decode
 from wex.url import URL
+
+log = logging.getLogger(__name__)
 
 
 def urls_from_robots_txt(response):
@@ -44,33 +47,37 @@ def urls_from_urlset_or_sitemapindex(response):
 
     sitemap = URL(response.url).fragment_dict.get('sitemap')
     content_subtypes = response.headers.get_content_subtype().split('+')
-    if not sitemap and not 'xml' in content_subtypes:
+    if not sitemap and 'xml' not in content_subtypes:
         return
 
     root = None
-    for _, elem in iterparse(decode(response)):
+    try:
+        for _, elem in iterparse(decode(response)):
 
-        if root is None:
-            root = elem.getroottree().getroot()
-            if not (root.tag.endswith('}sitemapindex') or
-                    root.tag.endswith('}urlset')):
-                # root element has wrong tag - give up
-                break
+            if root is None:
+                root = elem.getroottree().getroot()
+                if not (root.tag.endswith('}sitemapindex') or
+                        root.tag.endswith('}urlset')):
+                    # root element has wrong tag - give up
+                    break
 
-        if elem.tag.endswith('}loc') and elem.text is not None:
-            text = elem.text.strip()
-            if text:
-                # http://www.sitemaps.org/protocol.html#locdef
-                url = URL(urljoin(response.url, text))
-                if elem.getparent().tag.endswith('}sitemap'):
-                    # set sitemap=True to help downstream processing
-                    url = url.update_fragment_dict(sitemap=True)
-                yield "url", url
+            if elem.tag.endswith('}loc') and elem.text is not None:
+                text = elem.text.strip()
+                if text:
+                    # http://www.sitemaps.org/protocol.html#locdef
+                    url = URL(urljoin(response.url, text))
+                    if elem.getparent().tag.endswith('}sitemap'):
+                        # set sitemap=True to help downstream processing
+                        url = url.update_fragment_dict(sitemap=True)
+                    yield "url", url
 
-        if elem.getparent() is root:
-            # release memory for previous elements
-            while elem.getprevious() is not None:
-                del root[0]
+            if elem.getparent() is root:
+                # release memory for previous elements
+                while elem.getprevious() is not None:
+                    del root[0]
+
+    except XMLSyntaxError:
+        log.debug("XMLSyntaxError in '%s' (%d)", response.url, response.code)
 
 #: Extractor that combines :func:`.urls_from_robots_txt` and
 #: :func:`.urls_from_urlset_or_sitemapindex`.
