8a9f368ea0ddede9a5211f7dc4ed3689c51870ff
Giles Brown <gsbrown@ebay.com>
2015-08-18 11:53:52 -0700

    Last try with travis

--- tests/test_etree.py
+++ tests/test_etree.py
@@ -69,7 +69,7 @@ def test_parse_next_decoder():
     response = Response.from_readable(readable)
     # here the parse function will try utf-8 and then shift-jis
     etree = e.parse(response)
-    assert etree.xpath('//p/text()') == ['巨', '\n']
+    assert list(etree.getroot().itertext()) == ['\n', '巨', '\n']
 
 
 def test_parse_unreadable():
